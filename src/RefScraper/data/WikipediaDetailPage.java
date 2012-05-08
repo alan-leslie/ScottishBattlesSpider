83242df373fbf6b19233233401f0bfa8bbd32038
Alan L <alanlesli@gmail.com>
2012-05-08 16:44:33 +0100

    use correct code format

--- src/RefScraper/data/WikipediaDetailPage.java
+++ src/RefScraper/data/WikipediaDetailPage.java
@@ -373,4 +373,48 @@ public class WikipediaDetailPage {
 
         return dataValueNode;
     }
+    
+        static char asciiFromUTF(int codePoint) {
+        char retVal = ' ';
+        switch (codePoint) {
+            case 8211:
+                retVal = '-';
+                break;
+            case 232:
+                retVal = 'e';
+                break;
+        }
+
+        return retVal;
+    }
+
+    public static String getAsciiText(String theText) {
+        StringBuilder theBuilder = new StringBuilder();
+        int lengthInChars = theText.length();
+        int noOfCodePoints = theText.codePointCount(0, lengthInChars - 1);
+
+        try {
+            if (lengthInChars > 0
+                    && lengthInChars > noOfCodePoints) {
+                for (int offset = 0; offset < lengthInChars;) {
+                    final int codePoint = theText.codePointAt(offset);
+                    char theCharAt = theText.charAt(offset);
+
+                    if (codePoint >= 0 && codePoint < 128) {
+                        theBuilder.append(theCharAt);
+                    } else {
+                        theBuilder.append(WikipediaDetailPage.asciiFromUTF(codePoint));
+                    }
+
+                    offset += Character.charCount(codePoint);
+                }
+            } else {
+                theBuilder.append(theText);
+            }
+        } catch (IndexOutOfBoundsException exc) {
+            System.out.println("out of bounds");
+        }
+
+        return theBuilder.toString();
+    }
 }
